99aadce2cd3a9e2b92e8e6c9ffec73184f23d06b
ELdiyar Musaev <eldiyar.musaev@iaau.edu.kg>
2015-02-16 20:47:25 +0600

    first commit

---
+++ hello.rb
@@ -0,0 +1,29 @@
+def greet(variable)
+=begin
+	(string) -> string
+	
+	this method has to accept string
+	and return string
+
+	>  greet("Azamat")
+	=> "Hello, Azamat!"
+=end
+	# write your code here
+  "Hello, #{variable}!"
+end
+
+
+def hello
+=begin
+	() -> string
+
+	this method does not have to accept
+	any arguments and it has to return
+	string
+
+	>  hello()
+	=> "Hello!"
+=end
+	# write your code here
+  "Hello!"
+end
